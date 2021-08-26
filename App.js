e039aa0bfc9b8eca8a76e65aa262a29eeaf1c308
Gabriel Oliveira <g4brieloliveira.dev@gmail.com>
2021-08-26 01:13:14 -0300

    Create the App

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text></Text>
     </View>
   );
 }
